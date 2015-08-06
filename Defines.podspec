4680591d8d20107a503dd47cb8aedae0fa00484b
Bell App Lab <apps@bellapplab.com>
2015-08-06 18:15:19 +0100

    Fixing public variables

--- Defines.podspec
+++ Defines.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "Defines"
-  s.version          = "0.1.1"
+  s.version          = "0.1.2"
   s.summary          = "A small collection of Swift structs to replace #define statements in Obj-C."
   s.homepage         = "https://github.com/BellAppLab/Defines"
   s.license          = 'MIT'
--- Pod/Classes/Defines.swift
+++ Pod/Classes/Defines.swift
@@ -2,34 +2,34 @@ import UIKit
 
 public struct Screen
 {
-    static let Width = UIScreen.mainScreen().bounds.size.width
-    static let Height = UIScreen.mainScreen().bounds.size.height
-    static let MaxLength = max(Screen.Width, Screen.Height)
-    static let MinLength = min(Screen.Width, Screen.Height)
-    static let IsRetina = UIScreen.mainScreen().scale > 1
+    public static let Width = UIScreen.mainScreen().bounds.size.width
+    public static let Height = UIScreen.mainScreen().bounds.size.height
+    public static let MaxLength = max(Screen.Width, Screen.Height)
+    public static let MinLength = min(Screen.Width, Screen.Height)
+    public static let IsRetina = UIScreen.mainScreen().scale > 1
 }
 
 public struct Device
 {
-    static let IsiPad = UIDevice.currentDevice().userInterfaceIdiom == .Pad
-    static let IsiPhone4OrLess =  !IsiPad && Screen.MaxLength < 568.0
-    static let IsiPhone5 = !IsiPad && Screen.MaxLength == 568.0
-    static let IsiPhone6 = !IsiPad && Screen.MaxLength == 667.0
-    static let IsiPhone6p = !IsiPad && Screen.MaxLength == 736.0
+    public static let IsiPad = UIDevice.currentDevice().userInterfaceIdiom == .Pad
+    public static let IsiPhone4OrLess =  !IsiPad && Screen.MaxLength < 568.0
+    public static let IsiPhone5 = !IsiPad && Screen.MaxLength == 568.0
+    public static let IsiPhone6 = !IsiPad && Screen.MaxLength == 667.0
+    public static let IsiPhone6p = !IsiPad && Screen.MaxLength == 736.0
 }
 
 public struct Version
 {
-    static let IsiOS7 = (UIDevice.currentDevice().systemVersion as NSString).doubleValue < 8.0
-    static let IsiOS8 = (UIDevice.currentDevice().systemVersion as NSString).doubleValue < 9.0
-    static let IsiOS9 = (UIDevice.currentDevice().systemVersion as NSString).doubleValue < 10.0
+    public static let IsiOS7 = (UIDevice.currentDevice().systemVersion as NSString).doubleValue < 8.0
+    public static let IsiOS8 = (UIDevice.currentDevice().systemVersion as NSString).doubleValue < 9.0
+    public static let IsiOS9 = (UIDevice.currentDevice().systemVersion as NSString).doubleValue < 10.0
 }
 
 public struct App
 {
-    static let IsScaledUp = Device.IsiPhone6p && UIScreen.mainScreen().nativeScale > UIScreen.mainScreen().scale
-    static func BundleId() -> String {
+    public static let IsScaledUp = Device.IsiPhone6p && UIScreen.mainScreen().nativeScale > UIScreen.mainScreen().scale
+    public static func BundleId() -> String {
         return NSBundle.mainBundle().bundleIdentifier!
     }
-    static let Name = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleDisplayName") as! String
+    public static let Name = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleDisplayName") as! String
 }
